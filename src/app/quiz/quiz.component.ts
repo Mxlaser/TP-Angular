b3f0c4cc56b21db79551392f4b8dd51dccef6d79
Nikola.Z <nzivkovic@eig.fr>
2025-11-05 10:38:43 +0100

    commit Niko

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { QuizService } from "../shared/services/quiz.service";
+import { CategoriesService, Category } from "../shared/services/categories.service";
 
 @Component({
   selector: 'app-quiz',
@@ -11,17 +12,22 @@ import { QuizService } from "../shared/services/quiz.service";
 export class QuizComponent implements OnInit {
   isQuizFinished = this.quizService.isQuizFinished;
   playerName = '';
+  categoryId = 0;
+  category?: Category;
 
   constructor(
     private quizService: QuizService,
     private router: Router,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private categories: CategoriesService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.quizService.playerName = params['playerName'];
       this.playerName = params['playerName'];
+      this.quizService.playerName = this.playerName;
+      this.categoryId = Number(params['categoryId']);
+      this.categories.getById(this.categoryId).subscribe(c => this.category = c);
     });
   }
 
